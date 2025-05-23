f7a186b92279d2bfeb77bc2ade19c9253d18db1d
RoyZiman <76656437+RoyZiman@users.noreply.github.com>
2025-05-23 03:59:53 +0300

    Add custom ProfilePictureImageView for enhanced profile picture handling

---
+++ app/src/main/java/dev/android/project/ui/custom/ProfilePictureImageView.java
@@ -0,0 +1,86 @@
+package dev.android.project.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import androidx.cardview.widget.CardView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class ProfilePictureImageView extends CardView
+{
+    private static final int FOREGROUND_ATTR = android.R.attr.selectableItemBackground;
+    private ShapeableImageView imageView;
+
+    public ProfilePictureImageView(Context context)
+    {
+        super(context);
+        init(context);
+    }
+
+    private void init(Context context)
+    {
+        int size = isInEditMode() ? 100 : getResources().getDimensionPixelSize(
+                context.getResources().getIdentifier("profile_picture_size", "dimen", context.getPackageName())
+        );
+        setRadius((float)size / 2);
+        LayoutParams params = new LayoutParams(size, size);
+        setLayoutParams(params);
+        setCardElevation(0);
+        setUseCompatPadding(true);
+
+        imageView = new ShapeableImageView(context);
+        imageView.setLayoutParams(params);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setShapeAppearanceModel(
+                imageView.getShapeAppearanceModel()
+                         .toBuilder()
+                         .build()
+        );
+        addView(imageView);
+        // Set selectable foreground
+        int[] attrs = new int[] {FOREGROUND_ATTR};
+        android.content.res.TypedArray ta = context.obtainStyledAttributes(attrs);
+        int foregroundResId = ta.getResourceId(0, 0);
+        ta.recycle();
+        setForeground(foregroundResId != 0 ? context.getDrawable(foregroundResId) : null);
+
+        imageView.setImageResource(
+                context.getResources().getIdentifier("ic_nav_profile", "drawable", context.getPackageName())
+        );
+    }
+
+    public ProfilePictureImageView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ProfilePictureImageView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public void setImageResource(int resId)
+    {
+        imageView.setImageResource(resId);
+    }
+
+    public void setImageBitmap(android.graphics.Bitmap bitmap)
+    {
+        imageView.setImageBitmap(bitmap);
+    }
+
+    public void setImageDrawable(android.graphics.drawable.Drawable drawable)
+    {
+        imageView.setImageDrawable(drawable);
+    }
+
+    public ShapeableImageView getImageView()
+    {
+        return imageView;
+    }
+
+}
