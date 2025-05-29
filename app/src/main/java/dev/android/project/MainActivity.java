80767cc7c53267763282a12e817f7c0e69ee458c
RoyZiman <76656437+RoyZiman@users.noreply.github.com>
2025-05-29 04:08:20 +0300

    Show/hide inbox menu item based on user login status

--- app/src/main/java/dev/android/project/MainActivity.java
+++ app/src/main/java/dev/android/project/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity
         if (User.isLoggedIn())
         {
             navMenu.findItem(R.id.navProfile).setVisible(true);
+            navMenu.findItem(R.id.navInbox).setVisible(true);
             navMenu.findItem(R.id.navLogin).setVisible(false);
             navMenu.findItem(R.id.navLogout).setVisible(true)
                    .setOnMenuItemClickListener(item -> {
@@ -75,6 +76,7 @@ public class MainActivity extends AppCompatActivity
         else
         {
             navMenu.findItem(R.id.navProfile).setVisible(false);
+            navMenu.findItem(R.id.navInbox).setVisible(false);
             navMenu.findItem(R.id.navLogin).setVisible(true);
             navMenu.findItem(R.id.navLogout).setVisible(false);
         }
